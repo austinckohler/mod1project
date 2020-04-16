2c3ddab3c5e932331ccfacaabc396700dd799a1c
austinckohler <austinckohler@gmail.com>
2020-04-16 14:30:03 -0600

    used global vaariable, better usage of AR, cleaned up code

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -1,23 +1,23 @@
 
 class Cli  #shouldn't inherit from active record because its not a table in the database
    
-   
+   $user_name = ""
     def welcome
         puts "Welcome to concert finder!"
         puts " "
         puts "What is your name?"
-        user_name = gets.chomp
-        collect_user_info(user_name)
+        $user_name = gets.chomp
+        collect_user_info
     end
 
-    def collect_user_info(user_name)
+    def collect_user_info
         puts "What genre of music would you like to see this weekend?"
         puts "country, hip hop, electronic or metal"
         user_genre = gets.chomp.capitalize
         puts " "
 
         if user_genre == "Metal"  || user_genre == "Country"  || user_genre == "Electronic" || user_genre == "Hip hop"        
-            puts "Thanks #{user_name}! Let's find you a #{user_genre} show this weekend!"
+            puts "Thanks #{$user_name}! Let's find you a #{user_genre} show this weekend!"
             puts " "
         else  
             puts " "
@@ -26,48 +26,50 @@ class Cli  #shouldn't inherit from active record because its not a table in the 
             puts " "
             user_genre = "Metal"  
         end
-        set_user(user_name, user_genre)
+        set_user(user_genre)
     end 
 
-    def set_user(user_name, user_genre)
-        TicketHolder.create(name: user_name, favorite_genre: user_genre)
-        select_concerts_by_genre(user_genre, user_name)
+    def set_user(user_genre)
+        TicketHolder.create(name: $user_name, favorite_genre: user_genre)
+        select_concerts_by_genre(user_genre)
     end 
     
-    def select_concerts_by_genre(user_genre, user_name)
+    def select_concerts_by_genre(user_genre)
         results = Concert.select do |concert|
             concert.genre == user_genre
         end
         results
-        displays_concert_information(results, user_name)
+        displays_concert_information(results)
     end
 
-    def displays_concert_information(results, user_name)
+    def displays_concert_information(results)
         results.each do |concert|
-            venue_name = Venue.all.select do |venue|
-                 venue.id == concert.venue_id
-            end
-            venue1 = venue_name 
-            venue1[0].name
-            puts "There is a #{concert.artist} show at #{venue1[0].name} this #{concert.day} " 
+            puts "There is a #{concert.artist} show at #{concert.venue.name} this #{concert.day} \n" 
         end  
-        puts " "
-        play_again(user_name)
+        puts "\n"
+        user_selection(results)
     end
 
-    def play_again(user_name)
+    def user_selection(results)
+        puts "What band would you like to see?"
+        selection = gets.chomp 
+        # if selection.downcase == 
+        play_again
+    end
+
+    def play_again
             puts "Would you like to look for more concerts?"
             answer = gets.chomp.downcase
             if answer == "yes"
                 puts " "
-                self.repeat(user_name)
+                self.repeat
             else
                 puts " "
                 puts "Enjoy the show!!"
             end
     end
       
-    def repeat(user_name)
-            collect_user_info(user_name)
+    def repeat
+            collect_user_info
     end
 end
