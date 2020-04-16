7b169b8aeac699d428515f9fad2c65a6799d6acd
austinckohler <austinckohler@gmail.com>
2020-04-16 13:44:59 -0600

    formatted code for a cleaner reader experience

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -10,17 +10,15 @@ class Cli  #shouldn't inherit from active record because its not a table in the 
         collect_user_info(user_name)
     end
 
-    def repeat(user_name)
-        collect_user_info(user_name)
-    end
-
     def collect_user_info(user_name)
         puts "What genre of music would you like to see this weekend?"
         puts "country, hip hop, electronic or metal"
         user_genre = gets.chomp.capitalize
+        puts " "
 
         if user_genre == "Metal"  || user_genre == "Country"  || user_genre == "Electronic" || user_genre == "Hip hop"        
             puts "Thanks #{user_name}! Let's find you a #{user_genre} show this weekend!"
+            puts " "
         else  
             puts " "
             puts "No, it should be Metal... From now on you listen to Metal"
@@ -33,35 +31,31 @@ class Cli  #shouldn't inherit from active record because its not a table in the 
 
     def set_user(user_name, user_genre)
         TicketHolder.create(name: user_name, favorite_genre: user_genre)
-        select_matching_concerts(user_genre, user_name)
+        select_concerts_by_genre(user_genre, user_name)
     end 
     
-    def select_matching_concerts(user_genre, user_name)
+    def select_concerts_by_genre(user_genre, user_name)
         results = Concert.select do |concert|
             concert.genre == user_genre
         end
         results
-        display_matching_concerts(results, user_name)
+        displays_concert_information(results, user_name)
     end
 
-    def display_matching_concerts(results, user_name)
+    def displays_concert_information(results, user_name)
         results.each do |concert|
             venue_name = Venue.all.select do |venue|
                  venue.id == concert.venue_id
             end
-            
             venue1 = venue_name 
             venue1[0].name
             puts "There is a #{concert.artist} show at #{venue1[0].name} this #{concert.day} " 
-        
-            # selection_for_more_info
-            
         end  
         puts " "
         play_again(user_name)
     end
 
-        def play_again(user_name)
+    def play_again(user_name)
             puts "Would you like to look for more concerts?"
             answer = gets.chomp.downcase
             if answer == "yes"
@@ -71,13 +65,9 @@ class Cli  #shouldn't inherit from active record because its not a table in the 
                 puts " "
                 puts "Enjoy the show!!"
             end
-        end
+    end
       
-    
-
-    # def self.selection_for_more_info
-    #     puts "Please select a venue for more information"
-    #     selection = gets.chomp
-
-    # end 
+    def repeat(user_name)
+            collect_user_info(user_name)
+    end
 end
--- runner.rb
+++ runner.rb
@@ -4,5 +4,5 @@ require_relative 'config/environment'
 concert_app = Cli.new
 
 concert_app.welcome
-# concert_app.collect_user_info
-# binding.pry
+
+binding.pry
